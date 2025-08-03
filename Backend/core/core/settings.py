e92b45e6a7cfa7391af2ef1fd7ed797e42e8435e
Vaileti Mahesh Kumar <maheshkumarvmk0@gmail.com>
2025-08-03 15:18:37 +0530

    Updated Requirements

--- Backend/core/core/settings.py
+++ Backend/core/core/settings.py
@@ -123,6 +123,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8001",  # FastAPI
     "http://127.0.0.1:8001",
+    os.getenv('FASTAPI_URL', "https://emofelix-fastapi.onrender.com"),
+    os.getenv('FRONTEND_URL', "https://emofelix.netlify.app")
 ]
 
 CORS_ALLOW_CREDENTIALS = True
