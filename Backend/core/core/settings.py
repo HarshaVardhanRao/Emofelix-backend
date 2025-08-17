92a02cde85a741cd860ce57f6ba3295515efe688
Vaileti Mahesh Kumar <maheshkumarvmk0@gmail.com>
2025-08-17 21:28:41 +0530

    Updated

--- Backend/core/core/settings.py
+++ Backend/core/core/settings.py
@@ -188,3 +188,6 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 GEMINI_MODEL_NAME = os.getenv('GEMINI_MODEL_NAME', 'gemini-1.5-flash')
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://emofelix-backend.onrender.com",
+]
